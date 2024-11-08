792403bae6ed7b035ebf15d5bbe23e8e45092c33
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-11-08 07:31:44 +0100

    Capitalize first letters

--- Alphabet_Rangoli.py
+++ Alphabet_Rangoli.py
@@ -10,9 +10,10 @@ def print_rangoli(size):
         letters = alpha[i:size]
         # Create the row by reversing letters and adding forward letters (excluding first)
         row = letters[::-1] + letters[1:]
+        
         # Join with '-' and center the line
         line = '-'.join(row).center(4*size-3, '-')
-        lines.append(line)
+        lines.append(line)        
     
     # Print the pattern (top half + bottom half)
     print('\n'.join(lines[::-1] + lines[1:]))
---
+++ Capitalize_first_letters.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+     words=s.split()
+     capitalized_words=[word[0].upper()+word[1:].lower() for word in words]
+     capitalized_text=" ".join(capitalized_words)
+     return capitalized_text
+
+if __name__ == '__main__':
+    #os.environ['OUTPUT_PATH'] = 'output.txt'  # Or another file name
+    fptr =open('output.txt', 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
