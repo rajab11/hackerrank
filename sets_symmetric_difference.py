2bd034e0d725c01786892c5d73e3ba31939be25f
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-11-24 20:38:12 +0100

    Total number of students who have subscriptions to the English or the French newspaper but not both.

---
+++ sets_symmetric_difference.py
@@ -0,0 +1,46 @@
+"""
+Students of District College have subscriptions to English and French newspapers. Some students have subscribed to English only, some have subscribed to French only, and some have subscribed to both newspapers.
+
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to either the English or the French newspaper but not both.
+
+Input Format
+
+The first line contains the number of students who have subscribed to the English newspaper.
+The second line contains the space separated list of student roll numbers who have subscribed to the English newspaper.
+The third line contains the number of students who have subscribed to the French newspaper.
+The fourth line contains the space separated list of student roll numbers who have subscribed to the French newspaper.
+
+Constraints
+
+
+Output Format
+
+Output total number of students who have subscriptions to the English or the French newspaper but not both.
+
+Sample Input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Sample Output
+
+8
+Explanation
+
+The roll numbers of students who have subscriptions to English or French newspapers but not both are:
+4,5,7,9,10,11,21 and 55 .
+Hence, the total is  students.
+"""
+english_count = int(input())
+english_subs = set(map(int, input().split()))
+
+# Input the number of students subscribed to the French newspaper
+french_count = int(input())
+french_subs = set(map(int, input().split()))
+
+# Use the difference method to find students subscribed only to English
+only_english = english_subs.difference(french_subs)
+
+# Output the number of such students
+print(len(only_english))
