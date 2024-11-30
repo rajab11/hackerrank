3f2292827b2b668e11a925eb356819ac39f01250
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-11-30 21:52:18 +0100

    Finding polar coordinates

---
+++ polar_coordinates.py
@@ -0,0 +1,35 @@
+"""
+You are given a complex . Your task is to convert it to polar coordinates.
+
+Input Format
+
+A single line containing the complex number . Note: complex() function can be used in python to convert the input as a complex number.
+
+Constraints
+
+Given number is a valid complex number
+
+Output Format
+
+Output two lines:
+The first line should contain the value of .
+The second line should contain the value of .
+
+Sample Input
+
+  1+2j
+Sample Output
+
+ 2.23606797749979 
+ 1.1071487177940904
+Note: The output should be correct up to 3 decimal places.
+
+"""
+import cmath
+
+user_input=input()
+n=complex(user_input)
+
+r,theta=cmath.polar(n)
+print(f"{r:.3f}")
+print(f"{theta:.3f}")
