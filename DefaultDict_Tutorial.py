b55f208af0e2a0ce0bfc2836d39d396c9bfe5ae8
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-12-19 12:05:51 +0100

    Refactoring defaultdict tutorial

--- DefaultDict_Tutorial.py
+++ DefaultDict_Tutorial.py
@@ -66,19 +66,16 @@ In the sample problem, if 'c' also appeared in word group , you would print .
 """
 from collections import defaultdict
 
-# Read input values
-n, m = map(int, input().split())
-group_a = defaultdict(list)
+n,m=map(int,input().split())
+group_a=defaultdict(list)
 
-# Populate Group A with word indices
-for i in range(1, n + 1):
-    word = input()
+for i in range(1,n+1):
+    word=input()
     group_a[word].append(i)
 
-# Process Group B and output results
 for _ in range(m):
-    word = input()
+    word=input()
     if word in group_a:
-        print(" ".join(map(str, group_a[word])))
+        print(" ".join(map(str,group_a[word])))
     else:
         print(-1)
