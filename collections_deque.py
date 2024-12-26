813c09690dde8957c641f260ab8cd78828f2dcbb
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-12-26 07:13:58 +0100

    Collections, Deques, added question details

--- collections_deque.py
+++ collections_deque.py
@@ -1,3 +1,32 @@
+"""
+Perform append, pop, popleft and appendleft methods on an empty deque .
+
+Input Format
+
+The first line contains an integer , the number of operations.
+The next  lines contains the space separated names of methods and their values.
+
+Constraints
+
+
+Output Format
+
+Print the space separated elements of deque .
+
+Sample Input
+
+6
+append 1
+append 2
+append 3
+appendleft 4
+pop
+popleft
+Sample Output
+
+1 2
+
+"""
 from collections import deque
 
 #input
