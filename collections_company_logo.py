45c91d8e1ee23f5171f8b3d16e478d24b49cbccd
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-12-29 22:50:39 +0100

    Company logo problem solution

--- collections_company_logo.py
+++ collections_company_logo.py
@@ -3,10 +3,15 @@ import os
 import random
 import re
 import sys
-from collections import counter
+from collections import Counter
 
 
 if __name__ == '__main__':
+    #input
     s = input()
 
+    char_count=Counter(s)
+    print(char_count)
 
+    #output
+    print(' '.join(f"{char} {count}" for char,count in char_count.items()))
