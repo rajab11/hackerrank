9f4bfcad9754a6e4c2eb72c46735dcaa1dd77250
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2025-01-07 21:55:58 +0100

    Piling up solution try 1

--- piling_up.py
+++ piling_up.py
@@ -1,32 +1,66 @@
-def can_stack_cubes(test_cases):
-    results = []
-    for blocks in test_cases:
-        stack = []
-        while blocks:
-            if blocks[0] >= blocks[-1]:
-                block = blocks.pop(0)  # Pick leftmost block
-            else:
-                block = blocks.pop()  # Pick rightmost block
-            
-            # Check if stack is valid
-            if not stack or stack[-1] >= block:
-                stack.append(block)
-            else:
-                results.append("No")
-                break
+"""
+There is a horizontal row of  cubes. The length of each cube is given. You need to create a new vertical pile of cubes. The new pile should follow these directions: if  is on top of  then .
+
+When stacking the cubes, you can only pick up either the leftmost or the rightmost cube each time. Print Yes if it is possible to stack the cubes. Otherwise, print No.
+
+Example
+
+Result: No
+
+After choosing the rightmost element, , choose the leftmost element, . After than, the choices are  and . These are both larger than the top block of size .
+
+
+Result: Yes
+
+Choose blocks from right to left in order to successfully stack the blocks.
+
+Input Format
+
+The first line contains a single integer , the number of test cases.
+For each test case, there are  lines.
+The first line of each test case contains , the number of cubes.
+The second line contains  space separated integers, denoting the sideLengths of each cube in that order.
+
+Constraints
+
+
+
+Output Format
+
+For each test case, output a single line containing either Yes or No.
+
+Sample Input
+
+STDIN        Function
+-----        --------
+2            T = 2
+6            blocks[] size n = 6
+4 3 2 1 3 4  blocks = [4, 3, 2, 1, 3, 4]
+3            blocks[] size n = 3
+1 3 2        blocks = [1, 3, 2]
+Sample Output
+
+Yes
+No
+Explanation
+
+In the first test case, pick in this order: left - , right - , left - , right - , left - , right - .
+In the second test case, no order gives an appropriate arrangement of vertical cubes.  will always come after either  or .
+
+"""
+
+#input
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    blocks=input().split()
+    for i in range(n//2):
+        left=blocks[i]
+        right=blocks[-1-i]
+        max_value=max(left,right)
+        if max_value>=blocks[i+1]:
+            print(max_value,blocks[i+1],'Yes')
         else:
-            results.append("Yes")
-    return results
-
-# Input processing
-T = int(input("Enter number of test cases: "))
-test_cases = []
-for _ in range(T):
-    n = int(input("Enter number of cubes: "))  # Number of cubes (not used directly)
-    blocks = list(map(int, input("Enter cube side lengths: ").split()))
-    test_cases.append(blocks)
-
-# Solve and print results
-results = can_stack_cubes(test_cases)
-for result in results:
-    print(result)
+            print('No')
+    
