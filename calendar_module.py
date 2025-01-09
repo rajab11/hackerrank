1d4822491da40ce10eef0b0c050df9ff21551e86
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2025-01-09 08:10:42 +0100

    calendar module question details

--- calendar_module.py
+++ calendar_module.py
@@ -1,3 +1,28 @@
+"""
+You are given a date. Your task is to find what the day is on that date.
+
+Input Format
+
+A single line of input containing the space separated month, day and year, respectively, in    format.
+
+Constraints
+
+Output Format
+
+Output the correct day in capital letters.
+
+Sample Input
+
+08 05 2015
+Sample Output
+
+WEDNESDAY
+Explanation
+
+The day on August th  was WEDNESDAY.
+
+
+"""
 import calendar
 
 month,day,year=(int(i) for i in input().split(' '))
