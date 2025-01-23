656d9df9165ae7e12d8de9afe75ab66a55c73071
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2025-01-23 04:34:32 +0100

    Incorrect regex adding question detail

--- Incorrect_Regex.py
+++ Incorrect_Regex.py
@@ -1,3 +1,33 @@
+"""
+You are given a string S.
+Your task is to find out whether s is a valid regex or not.
+
+Input Format
+
+The first line contains integer , the number of test cases.
+The next  lines contains the string .
+
+Constraints
+
+
+Output Format
+
+Print "True" or "False" for each test case without quotes.
+
+Sample Input
+
+2
+.*\+
+.*+
+Sample Output
+
+True
+False
+Explanation
+
+.*\+ : Valid regex.
+.*+: Has the error multiple repeat. Hence, it is invalid.
+"""
 import re
 t=int(input()) #number of test cases
 
