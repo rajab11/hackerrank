9092f5b4ccf3c02da366b4af631bf7d36bad7c70
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2025-01-24 07:12:28 +0100

    find the torsional angle first attempt to solve problem

--- Find_the_Torsional_Angle.py
+++ Find_the_Torsional_Angle.py
@@ -2,12 +2,16 @@ import math
 
 class Points(object):
     def __init__(self, x, y, z):
-
+        return self.x+self.y+self.z
+    
     def __sub__(self, no):
-
+        return self.x-self.y
+    
     def dot(self, no):
+        return self.x+self.y
 
     def cross(self, no):
+        return self.x-self.y
         
     def absolute(self):
         return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
@@ -17,3 +21,10 @@ if __name__ == '__main__':
     for i in range(4):
         a = list(map(float, input().split()))
         points.append(a)
+
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+
+    print("%.2f" % math.degrees(angle))
