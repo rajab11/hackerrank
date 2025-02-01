51b242248d2b4232a5eb69264c94b0fad9d2fc9d
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2025-02-01 21:07:47 +0100

    question detail

--- input.py
+++ input.py
@@ -1,3 +1,35 @@
+"""
+You are given a polynomial  of a single indeterminate (or variable), .
+You are also given the values of  and . Your task is to verify if .
+
+Constraints
+All coefficients of polynomial  are integers.
+ and  are also integers.
+
+Input Format
+
+The first line contains the space separated values of  and .
+The second line contains the polynomial .
+
+Output Format
+
+Print True if . Otherwise, print False.
+
+Sample Input
+
+1 4
+x**3 + x**2 + x + 1
+Sample Output
+
+True
+Explanation
+
+
+Hence, the output is True.
+
+
+
+"""
 x,k=map(int,input().split()) #single indeterminate
 
 
